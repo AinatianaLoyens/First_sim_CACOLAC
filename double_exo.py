95b713e648c9601e8baddd30d166bbfd1f7dee84
Ainatiana <ainatianavic.loyens@gmail.com>
2025-02-25 17:00:57 +0100

    Creating double exo

---
+++ double_exo.py
@@ -0,0 +1,5 @@
+###This module is a module like exo.py but an exogenous mortality factor affects both x and y.
+
+#Pre-implemented functions that can be used in the models (like in exo.py)
+
+#General model with a discrete part for both x and y 
