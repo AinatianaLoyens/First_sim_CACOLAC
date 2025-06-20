ada8efa23e2161021805cb60ab9081bfde7c4d12
AinatianaLoyens <ainatianavic.loyens@gmail.com>
2025-06-20 13:37:29 +0200

    Simulations for new figures

--- double_exo.py
+++ double_exo.py
@@ -1245,7 +1245,7 @@ def compare_cont_imp_proportional_mortality_on_x(
                 plt.axvline(x=bound, color = 'gray', linestyle='dotted')
         if plot_eps:
             plt.plot(t, eps*np.ones_like(t), color = 'orange', linestyle=':')
-        plt.text(t[-1]*0.95, eps * 1.02, r'$\varepsilon$', color='orange', fontsize=14)
+            plt.text(t[-1]*0.95, eps * 1.02, r'$\varepsilon$', color='orange', fontsize=14)
         plt.xlabel('time')
         plt.ylabel('Population size')
         plt.title(f'Population of pests and predators with exogenous mortality on pests \n and the first impulsive exogenous mortality at t = {t_pulse} \n {kwargs_g}, {E_c = }, {T = }')
